078ec29659ec454fb1f3f8f2b87b30e3ccfd367c
JeonJunPyo02 <velvetviolet1024@naver.com>
2021-10-21 12:09:37 +0900

    Initial Commit

---
+++ Map_Tutorial/Map_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_TutorialApp.swift
+//  Map_Tutorial
+//
+//  Created by 전준표 on 2021/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Map_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Map_TutorialTests/Map_TutorialTests.swift
@@ -0,0 +1,33 @@
+//
+//  Map_TutorialTests.swift
+//  Map_TutorialTests
+//
+//  Created by 전준표 on 2021/10/21.
+//
+
+import XCTest
+@testable import Map_Tutorial
+
+class Map_TutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
